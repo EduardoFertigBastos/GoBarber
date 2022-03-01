6b9612dd668842af3e5223c67f1e84c3203a9cd4
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2022-03-01 01:00:18 -0300

    Routes

---
+++ web/src/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
+const RoutesStack: React.FC = () => (
+  <Routes>
+    {/* <Route path="/" element={<Dashboard />} /> */}
+    {/* <Route path="/repository/:pUser/:pRepository" element={<Repository />} /> */}
+  </Routes>
+)
+
+export default RoutesStack;
