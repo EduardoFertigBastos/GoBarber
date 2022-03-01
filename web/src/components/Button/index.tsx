b804a8d6f9b08c0afae011065cb1576d7a84875e
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2022-03-01 01:00:53 -0300

    Button Component

---
+++ web/src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import React, { ButtonHTMLAttributes } from 'react';
+import { Container } from './styles';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button: React.FC<ButtonProps> = ({ children, ...props}) => (
+  <button type="button" {...props}>
+    { children }
+  </button>
+);
+
+export default Button;
