358dbdb9272a5c4cd13a9d9f9ec44682e66603f3
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2022-03-03 00:06:53 -0300

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,11 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import RoutesStack from './routes';
 
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 )
