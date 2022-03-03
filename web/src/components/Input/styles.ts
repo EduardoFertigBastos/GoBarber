020bd1a52f3c6ac075fabcd48919727000edc528
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2022-03-03 00:07:04 -0300

    Componente Input

--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -1,6 +1,5 @@
 import styled, { css } from 'styled-components';
-
-// import Tooltip from '../Tooltip';
+import Tooltip from '../Tooltip';
 
 interface ContainerProps {
   isFocused: boolean;
@@ -8,7 +7,7 @@ interface ContainerProps {
   isErrored: boolean;
 }
 
-export const Container = styled.div`
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
   padding: 16px;
@@ -17,9 +16,30 @@ export const Container = styled.div`
   color: #666360;
   display: flex;
   align-items: center;
+
   & + div {
     margin-top: 8px;
   }
+
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
   input {
     flex: 1;
     background: transparent;
@@ -29,22 +49,23 @@ export const Container = styled.div`
       color: #666360;
     }
   }
+
   svg {
     margin-right: 16px;
   }
 `;
 
-// export const Error = styled(Tooltip)`
-//   height: 20px;
-//   margin-left: 16px;
-//   svg {
-//     margin: 0;
-//   }
-//   span {
-//     background: #c53030;
-//     color: #fff;
-//     &::before {
-//       border-color: #c53030 transparent;
-//     }
-//   }
-// `;
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+  span {
+    background: #c53030;
+    color: #fff;
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+`;
