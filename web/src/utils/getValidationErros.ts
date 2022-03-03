033e7fc61a3b7061b1974d42b750366666467615
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2022-03-03 00:07:48 -0300

    Validacao de Erros

---
+++ web/src/utils/getValidationErros.ts
@@ -0,0 +1,18 @@
+import { ValidationError } from 'yup';
+
+interface Errors {
+  [key: string]: string;
+}
+
+export function getValidationErrors(err: ValidationError): Errors {
+  const validationErrors:Errors = {};
+
+  err.inner.forEach((error) => {
+    if (typeof error.path !== 'undefined') {
+      validationErrors[error.path] = error.message;
+    }
+  })
+
+  return validationErrors;
+}
+
