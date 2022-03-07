8f4fb07d8a14fd7b1680465bd57d27c1313a89d9
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2022-03-07 00:42:15 -0300

    Route index

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 
+import { Switch } from 'react-router-dom';
 
-const RoutesStack: React.FC = () => (
-  <Routes>
-    {/* <Route path="/" element={<Dashboard />} /> */}
-    {/* <Route path="/repository/:pUser/:pRepository" element={<Repository />} /> */}
-  </Routes>
-)
+import Route from './Route';
 
-export default RoutesStack;
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={SignIn} />
+    <Route path="/signup" exact component={SignUp} />
+    <Route path="/forgot-password" exact component={ForgotPassword} />
+    <Route path="/reset-password" exact component={ResetPassword} />
+
+    <Route path="/dashboard" exact component={Dashboard} isPrivate />
+    <Route path="/profile" exact component={Profile} isPrivate />
+  </Switch>
+);
+
+export default Routes;
