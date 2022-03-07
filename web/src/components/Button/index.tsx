52a9cb6ddb6a4ca3b2da76b24c66f26a65da2add
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2022-03-07 00:43:13 -0300

    button

--- web/src/components/Button/index.tsx
+++ web/src/components/Button/index.tsx
@@ -1,12 +1,15 @@
 import React, { ButtonHTMLAttributes } from 'react';
+
 import { Container } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+};
 
-const Button: React.FC<ButtonProps> = ({ children, ...props}) => (
-  <button type="button" {...props}>
-    { children }
-  </button>
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
+  <Container type="button" {...rest}>
+    {loading ? 'Carregando...' : children}
+  </Container>
 );
 
 export default Button;
