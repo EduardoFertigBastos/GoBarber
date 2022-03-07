42ef1cb97ae770fd5812bbe5a27c94a2bfa4fae4
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2022-03-07 00:43:31 -0300

    toolti´p

--- web/src/components/Tooltip/styles.ts
+++ web/src/components/Tooltip/styles.ts
@@ -1,5 +1,4 @@
-import styled from "styled-components";
-
+import styled from 'styled-components';
 
 export const Container = styled.div`
   position: relative;
@@ -14,18 +13,21 @@ export const Container = styled.div`
     opacity: 0;
     transition: opacity 0.4s;
     visibility: hidden;
+
     position: absolute;
     bottom: calc(100% + 12px);
     left: 50%;
     transform: translateX(-50%);
+
     color: #312e38;
 
     &::before {
       content: '';
+      top: 100%;
       border-style: solid;
       border-color: #ff9000 transparent;
       border-width: 6px 6px 0 6px;
-      top: 100%;
+      bottom: 20px;
       position: absolute;
       left: 50%;
       transform: translateX(-50%);
