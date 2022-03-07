a7c06d77b15032944ad3fa67eebe45af52f5d9f1
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2022-03-07 00:42:21 -0300

    sign in

--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -1,82 +1,95 @@
-import React, { useRef, useCallback, useContext } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
-
-import { ValidationError } from 'yup';
-import { getValidationErrors } from '../../utils/getValidationErros';
-
-import logoImg from '../../assets/logo.svg';
 import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
+
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { useAuth } from '../../hooks/AuthContext';
 
-import { Container, Content, Background } from './styles';
+import getValidationsErrors from '../../utils/getValidationErrors';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 
-interface SignInCredentials {
+import logo from '../../assets/logo.svg';
+
+import { Container, Content, AnimationContainer, Background } from './styles';
+
+interface SignInFormData {
   email: string;
   password: string;
 }
 
 const SignIn: React.FC = () => {
-
+  const history = useHistory();
+  const { signIn } = useAuth();
+  const { addToast } = useToast();
   const formRef = useRef<FormHandles>(null);
 
-  const { user, signIn } = useAuth();
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório!')
+            .email('Digite um e-mail válido!'),
+          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+        });
 
-  console.log(user);
-  const handleSubmit = useCallback(async (data: SignInCredentials) => {
-    try {
-      formRef.current?.setErrors({});
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail obrigatório')
-          .email('Digite um e-mail válido'),
-        password: Yup.string()
-          .min(6, 'No mínimo 6 dígitos')
-      });
+        await schema.validate(data, {
+          abortEarly: false,
+        });
 
-      await schema.validate(data, {
-        abortEarly: false
-      });
+        await signIn({ ...data });
+        history.push('/dashboard');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationsErrors(err);
+          formRef.current?.setErrors(errors);
+        }
 
-      signIn({
-        email: data.email,
-        password: data.password
-      });
-
-    } catch (error) {
-      if (error instanceof ValidationError) {
-        formRef.current?.setErrors(getValidationErrors(error));
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação!',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+        });
       }
-      console.log(error)
-    }
-  }, [signIn]);
+    },
+    [history, signIn, addToast],
+  );
 
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="GoBarber" />
+        <AnimationContainer>
+          <img src={logo} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1> Faça o seu login </h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu logon</h1>
 
-          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
-          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+            <Input name="email" placeholder="E-mail" icon={FiMail} />
+            <Input
+              name="password"
+              type="password"
+              placeholder="Senha"
+              icon={FiLock}
+            />
+            <Button type="submit">Entrar</Button>
 
-          <Button type="submit">Entrar</Button>
+            <Link to="/forgot-password">Esqueci minha senha</Link>
+          </Form>
 
-          <a href="forgot">Esqueci a minha senha</a>
-        </Form>
-
-        <a href="/login"> <FiLogIn /> Criar conta</a>
+          <Link to="/signup">
+            <FiLogIn />
+            Criar conta
+          </Link>
+        </AnimationContainer>
       </Content>
-
       <Background />
     </Container>
   );
-}
+};
 
 export default SignIn;
--- web/src/pages/SignIn/styles.ts
+++ web/src/pages/SignIn/styles.ts
@@ -8,6 +8,7 @@ from {
   opacity: 0;
   transform: translateX(-50px);
 }
+
 to {
   opacity: 1;
   transform: translateX(0);
@@ -16,6 +17,7 @@ to {
 
 export const Container = styled.div`
   height: 100vh;
+
   display: flex;
   align-items: stretch;
 `;
@@ -24,9 +26,19 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
   place-content: center;
+
   width: 100%;
   max-width: 700px;
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  animation: ${appearFromLeft} 1s;
 
   form {
     margin: 80px 0;
@@ -35,33 +47,6 @@ export const Content = styled.div`
 
     h1 {
       margin-bottom: 24px;
-    }
-
-    input {
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      width: 100%;
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button {
-      background: #ff9000;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      color: #312e38;
-      width: 100%;
-      font-weight: 500;
-      margin-top: 16px;
-
-      &:hover {
-        color: ${shade(0.2, '#ff9000')};
-      }
     }
 
     a {
@@ -78,20 +63,22 @@ export const Content = styled.div`
   }
 
   > a {
-      color: #ff9000;
-      display: block;
-      margin-top: 24px;
-      text-decoration: none;
-      transition: color 0.2s;
-      display: flex;
-      align-items: center;
-      &:hover {
-        color: ${shade(0.2, '#ff9000')};
-      }
-      svg {
-        margin-right: 16px;
-      }
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+    display: flex;
+    align-items: center;
+
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
     }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
 `;
 
 export const Background = styled.div`
